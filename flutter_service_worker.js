b9e458c6e51fed9944cf2f5a0995904739cb193a
TseWei-andy <andy24936997@gmail.com>
2026-03-06 23:16:34 +0800

    初次發布停車助手網頁版 (v1.0)
    這是使用 Flutter 編譯的正式版本，包含跑車背景底圖、定位存儲與地圖導航功能。

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e36b739f38a4b6e58ea9d6a20127e4f8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "7d907f8e620b72f3d23ad9f1c972b33b",
+"flutter_bootstrap.js": "0a2834feb380143b4046795ec371407a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
